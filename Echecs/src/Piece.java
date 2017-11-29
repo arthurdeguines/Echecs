cf8e0fac202246f414669d06c3721cd86a412e31
Arthur Deguines <deguinesarthur@gmail.com>
2017-11-29 21:51:37 +0100

    Init

---
+++ Echecs/src/Piece.java
@@ -0,0 +1,10 @@
+
+public class Piece {
+	String nom = "init";
+	public Piece() {
+		
+	}
+	public String toString() {
+		return this.nom;
+	}
+}
---
+++ Echecs/src/Pion.java
@@ -0,0 +1,9 @@
+
+public class Pion extends Piece{
+	public Pion(){
+		nom = "Pion";
+	}
+	public String toString() {
+		return nom;
+	}
+}
